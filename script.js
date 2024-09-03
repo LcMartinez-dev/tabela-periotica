9e14cbd6f8e2d5d767cfc9c6a1cb715a7087ec9f
Lucas Martini <martinilucas2021@gmail.com>
2024-09-03 08:17:55 -0300

    TP v1.0

---
+++ script.js
@@ -0,0 +1,11 @@
+<div class="tabela">
+<div id="Alca" class="f18"></div>
+<div id="terra" class="repre"></div>
+<div id="trans" class="meta"></div>
+<div id="boro" class="repre"></div>
+<div id="carbo" class="repre"></div>
+<div id="nitro" class="repre"></div>
+<div id="halo" class="repre"></div>
+<div id="calco" class="repre"></div>
+<div id="gasNo" class="f18"></div>
+</div>
